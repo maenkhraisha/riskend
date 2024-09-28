32450b0ae01f6b21ada40a3914629e8d680ca60f
Maen KH <m.alkhraisha@sakan.co>
2024-09-29 01:51:19 +0300

    cros origion

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://riskend.onrender.com"],
         credentials: true,
     })
 );
