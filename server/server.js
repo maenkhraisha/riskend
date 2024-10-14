5453ccd10a07c771a082b01cff51fb7c5857c322
Maen KH <m.alkhraisha@sakan.co>
2024-10-14 22:23:13 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.use(
     cors({
         origin: ["http://localhost:3000", "https://riskend-client.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+        allowedHeaders: [
+            "Content-Type",
+            "Origin",
+            "X-Requested-With",
+            "Accept",
+            "x-client-key",
+            "x-client-token",
+            "x-client-secret",
+            "Authorization",
+        ],
         credentials: true,
     })
 );
