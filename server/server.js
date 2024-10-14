54e6e3439dfac3546f7315df12ec07015a051ef7
Maen KH <m.alkhraisha@sakan.co>
2024-10-15 00:57:36 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -37,10 +37,7 @@ const app = express();
 // app.use(cors(corsoptions));
 // app.options("*", cors(corsoptions));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", [
-        "http://localhost:3000",
-        "https://riskend-client.onrender.com",
-    ]); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
