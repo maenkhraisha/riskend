eded7d4428670626626eebb041e7ca2ba42294d0
Maen KH <m.alkhraisha@sakan.co>
2024-10-16 20:20:23 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -18,29 +18,11 @@ import { cusRefreshRouter } from "./routes/cusRefreshRoute.js";
 dotenv.config();
 const app = express();
 
-// const corsoptions = {
-//     origin: ["http://localhost:3000", "https://riskend-client.onrender.com:3000"],
-//     credentials: true,
-// };
-// app.use(cors(corsoptions));
-
-var allowedOrigins = ["http://localhost:3000", "https://riskend-client.onrender.com"];
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg =
-                    "The CORS policy for this site does not " +
-                    "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
+const corsoptions = {
+    origin: ["http://localhost:3000", "https://riskend-client.onrender.com"],
+    credentials: true,
+};
+app.use(cors(corsoptions));
 
 app.use(cookieParser());
 app.use(express.json());
